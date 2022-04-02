adb26ff50d34caab00ee1706319ca6d02ec196ba
bnznamco <gabriele.baldi.01@gmail.com>
2022-04-02 21:58:37 +0200

    feat: new auth and cookies middleware This update permits to mainain a better isolation between mapo and camomilla sessions

---
+++ packages/api-server/api/middlewares/auth.js
@@ -0,0 +1,40 @@
+const libCookie = require('cookie');
+const setCookie = require('set-cookie-parser');
+
+
+const mapAuthLogicFactory = ({ sync }) => (proxyRes, req, res) => {
+    const paths = [
+        "/api/camomilla/auth/login/",
+        "/api/camomilla/auth/logout/"
+    ]
+    if (paths.includes(req.path)) {
+        const cookies = setCookie.parse(proxyRes);
+        const sessionid = cookies.find(c => c.name == "sessionid")
+        const __mapo_session = cookies.find(c => c.name == "__mapo_session")
+        if (sessionid && !__mapo_session) cookies.push({ ...sessionid, name: "__mapo_session", secure: true })
+        proxyRes.headers['set-cookie'] = cookies.filter(c => sync || c.name !== 'sessionid').map(function (cookie) {
+            return libCookie.serialize(cookie.name, cookie.value, cookie);
+        });
+    }
+}
+
+const mapCookiesFactory = ({ sync }) => (req, res, next) => {
+    const { csrftoken, __mapo_session } = req.cookies
+    if (__mapo_session) req.cookies.sessionid = __mapo_session
+    if (csrftoken) req.headers['X-CSRFToken'] = csrftoken
+    if (!sync) req.headers['cookie'] = Object.keys(req.cookies).map(k => `${k}=${req.cookies[k]}`).join(";")
+    next()
+}
+
+const mapAuthLogic = mapAuthLogicFactory({ sync: false })
+const mapAuthLogicSync = mapAuthLogicFactory({ sync: true })
+const mapCookies = mapCookiesFactory({ sync: false })
+const mapCookiesSync = mapCookiesFactory({ sync: true })
+
+module.exports = {
+    mapAuthLogic,
+    mapAuthLogicSync,
+    mapCookies,
+    mapCookiesSync,
+
+}
--- packages/api-server/api/proxy/index.js
+++ packages/api-server/api/proxy/index.js
@@ -1,29 +1,30 @@
 const app = require('express')()
 const cookieParser = require("cookie-parser");
-const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware')
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
+const { mapAuthLogic, mapAuthLogicSync, mapCookies, mapCookiesSync } = require('../middlewares/auth.js');
 
 
 module.exports = (options) => {
-    const { 
+    const {
         target,
         headers,
         changeOrigin,
         pathRewrite,
         onProxyRes,
         onProxyReq,
+        use,
+        syncCamomillaSession
     } = options.api || {}
+
+    const authMiddleware = syncCamomillaSession ? mapAuthLogicSync : mapAuthLogic
+    const cookiesMiddleware = syncCamomillaSession ? mapCookiesSync : mapCookies
 
     const proxyOptions = {
         target,
         headers,
         changeOrigin: changeOrigin !== undefined ? changeOrigin : true,
-        onProxyReq: (proxyReq, req, res) => {
-            const { csrftoken } = req.cookies
-            if (csrftoken) proxyReq.setHeader('X-CSRFToken', csrftoken)
-            if (typeof onProxyReq === 'function') {
-                onProxyReq(proxyReq, req, res)
-            }
-        },
+        onProxyReq,
+        onProxyRes: authMiddleware,
         pathRewrite: {
             '^/api/auth/login': '/api/camomilla/auth/login/',
             '^/api/auth/logout': '/api/camomilla/auth/logout/',
@@ -31,14 +32,24 @@ module.exports = (options) => {
             '^/api/media': '/api/camomilla/media',
             '^/api/media-folder': '/api/camomilla/media-folder',
             ...pathRewrite
-        }
+        },
     }
 
     if (typeof onProxyRes === 'function') {
-        proxyOptions.onProxyRes = responseInterceptor(onProxyRes)
+        proxyOptions.onProxyRes = (proxyRes, req, res) => {
+            authMiddleware(proxyRes, req, res)
+            responseInterceptor(onProxyRes)(proxyRes, req, res)
+        }
     }
 
-    app.use(cookieParser())
+    if (typeof use === 'function') {
+        app.use(use);
+    } else if (Array.isArray(use)) {
+        use.filter(func => typeof func == "function").forEach(func => app.use(func));
+    }
+
+    app.use(cookieParser());
+    app.use(cookiesMiddleware);
     app.use(createProxyMiddleware(proxyOptions));
 
     return app
